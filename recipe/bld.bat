b1748cca3d6e18da37f2d28ae6af8bf1a9ff6c7c
leej3 <johnleenimh@gmail.com>
2022-04-19 10:01:34 +0100

    minimal fixes for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,11 +4,10 @@ cd build
 
 :: Configure
 cmake .. -LAH -GNinja                                ^
-  %CMAKE_ARGS%                                       ^
   -DBUILD_SHARED_LIBS:bool=on                        ^
   -DBUILD_STATIC_LIBS:bool=off                       ^
   -DCMAKE_BUILD_TYPE=Release                         ^
-  -DCMAKE_INSTALL_PREFIX:path=%PREFIX%               ^
+  -DCMAKE_INSTALL_PREFIX:path=%LIBRARY_PREFIX%       ^
   -DWITH_LCMS2:bool=on
 
 :: Build and install
